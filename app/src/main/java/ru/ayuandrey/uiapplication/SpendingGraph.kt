449d0c88035be0c0aa21023de9fa700da97a9b0e
andrey <andrey.ostapovich.90@mail.ru>
2024-07-28 12:57:38 +0300

    первая фиксация

---
+++ app/src/main/java/ru/ayuandrey/uiapplication/SpendingGraph.kt
@@ -0,0 +1,92 @@
+package ru.ayuandrey.uiapplication
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.DirectionsRun
+import androidx.compose.material.icons.rounded.Restaurant
+import androidx.compose.material.icons.rounded.ShoppingBag
+import androidx.compose.material.icons.rounded.Subscriptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.toFontFamily
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import me.bytebeats.views.charts.bar.BarChart
+import me.bytebeats.views.charts.bar.BarChartData
+import me.bytebeats.views.charts.bar.render.label.SimpleLabelDrawer
+import me.bytebeats.views.charts.bar.render.xaxis.SimpleXAxisDrawer
+import me.bytebeats.views.charts.bar.render.yaxis.SimpleYAxisDrawer
+
+
+@Composable
+fun SpendingGraph(
+    modifier: Modifier = Modifier
+) {
+    Text(
+        modifier = Modifier
+            .padding(horizontal = 22.dp),
+        text = "Spending Statistics",
+        fontFamily = Font(R.font.play).toFontFamily(),
+        fontSize = 25.sp
+    )
+
+    Spacer(modifier = Modifier.height(16.dp))
+
+    Chart(modifier = modifier)
+
+}
+
+
+@Composable
+fun Chart(modifier: Modifier = Modifier) {
+    BarChart(
+        modifier = modifier,
+        barChartData = BarChartData(spendingByDay),
+        xAxisDrawer = SimpleXAxisDrawer(
+            axisLineThickness = 2.dp,
+            axisLineColor = MaterialTheme.colorScheme.onBackground.copy(0.2f)
+        ),
+        yAxisDrawer = SimpleYAxisDrawer(
+            labelValueFormatter = {"$ ${it.toInt()}"},
+            axisLineThickness = 2.dp,
+            axisLineColor = MaterialTheme.colorScheme.onBackground.copy(0.2f)
+        ),
+        labelDrawer = SimpleLabelDrawer(
+            drawLocation = SimpleLabelDrawer.DrawLocation.Outside,
+            labelTextSize = 15.sp
+        )
+    )
+}
+
+val spendingByDay = listOf(
+    BarChartData.Bar(
+        label = "Dec 1",
+        value = 123f,
+        color = randomColor(50),
+    ),
+    BarChartData.Bar(
+        label = "Dec 2",
+        value = 160f,
+        color = randomColor(50),
+    ),
+    BarChartData.Bar(
+        label = "Dec 3",
+        value = 180f,
+        color = randomColor(50),
+    ),
+    BarChartData.Bar(
+        label = "Dec 4",
+        value = 23f,
+        color = randomColor(50),
+    ),
+    BarChartData.Bar(
+        label = "Dec 5",
+        value = 73f,
+        color = randomColor(50),
+    ),
+)
